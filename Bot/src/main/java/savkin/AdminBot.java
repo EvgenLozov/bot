95e2fff7d467a8c8d9bee26925bdf15c1cbcbd44
DmitrySavkin <dimsaz@mail.ru>
2018-08-01 18:58:01 +0200

    Try deploy

---
+++ Bot/src/main/java/savkin/AdminBot.java
@@ -0,0 +1,68 @@
+package savkin;
+
+import org.telegram.telegrambots.api.methods.ParseMode;
+import org.telegram.telegrambots.api.methods.send.SendMessage;
+import org.telegram.telegrambots.api.objects.Message;
+import org.telegram.telegrambots.api.objects.Update;
+import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.exceptions.TelegramApiException;
+
+public class AdminBot extends TelegramLongPollingBot {
+
+    private static AdminBot bot;
+
+    private AdminBot() {
+
+    }
+
+    public static AdminBot getBot() {
+        if (bot == null) {
+            bot = new AdminBot();
+
+        }
+        return bot;
+    }
+
+
+    public void onUpdateReceived(Update update) {
+
+    }
+
+
+    public String getBotUsername() {
+        return "newcustomerphonebot";
+    }
+
+
+    public String getBotToken() {
+        return "TOKEN";
+    }
+
+    public void sendMsg(Message msg, String text) {
+        SendMessage sm = new SendMessage()
+                .setChatId("331660513")
+                .setText(text).enableHtml(true);
+
+        try {
+            execute(sm);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void sendMsg(Message msg, String text, InlineKeyboardMarkup keyboardMarkup) {
+
+        SendMessage message = new SendMessage() // Create a message object object
+                .setChatId(msg.getChatId())
+                .setText(text);
+        message.setReplyMarkup(keyboardMarkup);
+        message.setParseMode(ParseMode.HTML);
+
+        try {
+            execute(message);
+        } catch (TelegramApiException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ Bot/src/main/java/savkin/DataReader.java
@@ -0,0 +1,122 @@
+package savkin;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.xml.sax.SAXException;
+
+import javax.xml.transform.Transformer;
+import java.io.File;
+import java.util.List;
+
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import javax.swing.text.BadLocationException;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+public class DataReader {
+
+
+    public final File file = new File(ClassLoader.getSystemResource("text.xml").getFile());
+
+
+    public final Document getContent() throws SAXException,
+            ParserConfigurationException,
+            IOException, BadLocationException,
+            org.xml.sax.SAXException {
+
+
+        DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
+                .newInstance();
+        DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+        Document document = documentBuilder.parse(this.file);
+
+        return document;
+    }
+
+
+    public Node getRootDocument() {
+        NodeList list;
+        return null;
+    }
+
+    public List<String> getTariffis() {
+        List<String>  samokats = new ArrayList();
+        try {
+
+            NodeList l = getContent().getElementsByTagName("data");
+            // System.out.println(l.getLength());
+
+            for (int i = 0; i < l.getLength(); i++) {
+                NodeList tInfo = l.item(i).getChildNodes();
+
+                //     System.out.println("j " + j + " " + tInfo.item(j).getTextContent() );
+                System.out.println(l.item(i).getTextContent());
+                samokats.add(l.item(i).getTextContent());
+            }
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        }
+
+        return samokats;
+    }
+
+
+    public void addData(String data) {
+
+        try {
+            Document doc = getContent();
+            Element root = doc.getDocumentElement();
+            Element samokatElement = doc.createElement("samokat");
+            Element dataElement = doc.createElement("data");
+            dataElement.appendChild(doc.createTextNode(data));
+            samokatElement.appendChild(dataElement);
+            root.appendChild(samokatElement);
+            TransformerFactory transformerFactory = TransformerFactory.newInstance();
+            Transformer transformer = transformerFactory.newTransformer();
+            DOMSource domSource = new DOMSource(doc);
+            StreamResult streamResult = new StreamResult(this.file);
+            transformer.transform(domSource, streamResult);
+
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (BadLocationException e) {
+            e.printStackTrace();
+        } catch (TransformerConfigurationException e) {
+            e.printStackTrace();
+        } catch (TransformerException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void main(String[] args) {
+        DataReader dr =  new DataReader();
+        dr.addData("31.09.18");
+        dr.getTariffis();
+    }
+}
